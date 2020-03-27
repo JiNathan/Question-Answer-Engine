1b2ea2b27b3c7e4d70c9112b7e72e964de2ed8c1
JiNathan <nathanji.business@gmail.com>
2020-03-26 18:19:43 -0700

    Split sentences correctly

--- QuestionAnswerEngineTask.py
+++ QuestionAnswerEngineTask.py
@@ -10,10 +10,10 @@ from spacy.pipeline import merge_entities
 from spacy.matcher import Matcher
 from spacy.tokens import Span
 from spacy import displacy
-
+nltk.download('punkt')
 pd.set_option('display.max_colwidth', 200)
 nlp = spacy.load("en_core_web_sm")
-text2 = 'President Obama accepted the Nobel Peace Prize during a ceremony Thursday in Norway, acknowledging the paradox of receiving the award as the US is embroiled in two wars, while maintaining that instruments of war have a role in preserving peace.In his acceptance speech, Obama told Nobel Committee members and guests in Oslo that achieving peace must begin with the recognition that the use of force is sometimes morally justified. Make no mistake: Evil does exist in the world. A nonviolent movement could not have halted Hitlers armies. Negotiations cannot convince al-Qaidas leaders to lay down their arms,"he told the crowd.It was just nine days ago that Obama announced he is sending an additional 30,000 U.S. troops to Afghanistan in an effort to step up training of Afghan security forces and root out insurgents operating on the border with Pakistan.'
+text2 = 'President Obama accepted the Nobel Peace Prize during a ceremony Thursday in Norway, acknowledging the paradox of receiving the award as the US is embroiled in two wars, while maintaining that instruments of war have a role in preserving peace. In his acceptance speech, Obama told Nobel Committee members and guests in Oslo that achieving peace must begin with the recognition that the use of force is sometimes morally justified. Make no mistake: Evil does exist in the world. A nonviolent movement could not have halted Hitlers armies. Negotiations cannot convince al-Qaidas leaders to lay down their arms,"he told the crowd. It was just nine days ago that Obama announced he is sending an additional 30,000 U.S. troops to Afghanistan in an effort to step up training of Afghan security forces and root out insurgents operating on the border with Pakistan.'
 # doc = nlp(text2)
 question = 'Where did obama accepted the Nobel Peace Prize'
 # questiondoc = nlp(question)
@@ -113,7 +113,7 @@ def giveAnswer(svo_list, questiondoc):
 def returnresult(text, question):
     questiondoc = nlp(question)
     sen_map = {}
-    sentences = text.split(".")
+    sentences = nltk.tokenize.sent_tokenize(text)
     for i in range(len(sentences)):
        tokened = nlp(sentences[i])
        sen_map[i] = svoMatcher(tokened)
@@ -126,8 +126,6 @@ def returnresult(text, question):
            highest_score = h
            highest_key = k
     return sentences[highest_score]
-
-
 
 print(returnresult(text2, question))
 
