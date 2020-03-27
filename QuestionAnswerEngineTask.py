1889821f6d9cbdf5cafdd8e62f4b5532e4c01a14
JiNathan <nathanji.business@gmail.com>
2020-03-26 18:42:06 -0700

    Three quote string test

--- QuestionAnswerEngineTask.py
+++ QuestionAnswerEngineTask.py
@@ -15,6 +15,7 @@ pd.set_option('display.max_colwidth', 200)
 nlp = spacy.load("en_core_web_sm")
 text2 = 'President Obama accepted the Nobel Peace Prize during a ceremony Thursday in Norway, acknowledging the paradox of receiving the award as the US is embroiled in two wars, while maintaining that instruments of war have a role in preserving peace. In his acceptance speech, Obama told Nobel Committee members and guests in Oslo that achieving peace must begin with the recognition that the use of force is sometimes morally justified. Make no mistake: Evil does exist in the world. A nonviolent movement could not have halted Hitlers armies. Negotiations cannot convince al-Qaidas leaders to lay down their arms,"he told the crowd. It was just nine days ago that Obama announced he is sending an additional 30,000 U.S. troops to Afghanistan in an effort to step up training of Afghan security forces and root out insurgents operating on the border with Pakistan.'
 # doc = nlp(text2)
+quotetext = """President Obama accepted the Nobel Peace Prize during a ceremony Thursday in Norway, acknowledging the paradox of receiving the award as the U.S. is embroiled in two wars, while maintaining that instruments of war have a role in preserving peace. In his acceptance speech, Obama told Nobel Committee members and guests in Oslo that achieving peace must begin with the recognition that the use of force is sometimes morally justified. Make no mistake: Evil does exist in the world. A nonviolent movement could not have halted Hitler's armies. Negotiations cannot convince al-Qaida's leaders to lay down their arms," he told the crowd. It was just nine days ago that Obama announced he is sending an additional 30,000 U.S. troops to Afghanistan in an effort to step up training of Afghan security forces and root out insurgents operating on the border with Pakistan."""
 question = 'Where did obama accepted the Nobel Peace Prize'
 # questiondoc = nlp(question)
 # nlp.add_pipe(merge_entities)
@@ -127,6 +128,7 @@ def returnresult(text, question):
            highest_key = k
     return sentences[highest_score]
 
-print(returnresult(text2, question))
-
-
+# print(returnresult(text2, question))
+print(returnresult(quotetext,question))
+print(quotetext)
+print(question)
