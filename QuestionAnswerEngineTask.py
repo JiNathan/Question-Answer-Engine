eb4fa55cac15652b0ee2e4433de1ce74e3b994ea
JiNathan <nathanji.business@gmail.com>
2020-04-23 16:03:45 -0700

    minor changes

--- QuestionAnswerEngineTask.py
+++ QuestionAnswerEngineTask.py
@@ -1,6 +1,7 @@
 import re
 import string
 import nltk
+from nltk.stem import WordNetLemmatizer
 import spacy
 import pandas as pd
 import numpy as np
@@ -10,6 +11,7 @@ from spacy.pipeline import merge_entities
 from spacy.matcher import Matcher
 from spacy.tokens import Span
 from spacy import displacy
+lemmatizer = WordNetLemmatizer()
 nltk.download('punkt')
 pd.set_option('display.max_colwidth', 200)
 nlp = spacy.load("en_core_web_sm")
@@ -28,6 +30,23 @@ question = 'Where did obama accepted the Nobel Peace Prize'
 # print token, dependency, POS tag
 # for tok in doc:
 #   print(tok.text, "-->",tok.dep_,"-->", tok.pos_)
+def pending(possiblepairs):
+    if len(possiblepairs[0]) > 0 and len(possiblepairs[1]) > 0:
+        return 2
+    elif len(possiblepairs[0]) > 0 or len(possiblepairs[1]) > 0:
+        return 1
+    else:
+        return 0
+
+def findnextitem(tok, possiblepairs, a):
+    if (tok.dep_ == 'nsubj' or tok.dep_ == 'nsubjpass') and len(possiblepairs[a]) == 0:
+        return(tok.text)
+    if (tok.pos_ == 'VERB' or tok.pos_ == 'AUX') and len(possiblepairs[a]) == 1 and tok.dep_ != 'aux' and tok.dep_ !='auxpass':
+        return(tok.text)
+    if (tok.dep_ == 'dobj' or tok.dep_ == 'pobj' or tok.dep_ == 'attr' or tok.dep_ == 'acomp') and len(
+            possiblepairs[a]) == 2:
+        return(tok.text)
+    return 'none'
 def contains(a, b):
     #checks substrings as well
     if a == b:
@@ -42,17 +61,24 @@ def contains(a, b):
         return False
 def svoMatcher(doc):
     svopairs = []
-    possiblepairs = []
+    possiblepairs = [[],[]]
+    pendingpairs = 1
     for tok in doc:
-       if (tok.dep_ == 'nsubj' or tok.dep_ == 'nsubjpass') and len(possiblepairs) == 0:
-           possiblepairs.append(tok.text)
-       if (tok.pos_ == 'VERB' or tok.pos_ == 'AUX' or tok.pos_ == 'AUXPASS') and len(possiblepairs) == 1:
-           possiblepairs.append(tok.text)
-       if (tok.dep_ == 'dobj' or tok.dep_ == 'pobj' or tok.dep_ == 'attr' or tok.dep_ == 'acomp') and len(possiblepairs) == 2:
-           possiblepairs.append(tok.text)
-       if len(possiblepairs) == 3:
-           svopairs.append(possiblepairs)
-           possiblepairs =[]
+        if findnextitem(tok, possiblepairs, 0) != 'none':
+            possiblepairs[0].append(findnextitem(tok, possiblepairs, 0))
+        else:
+            if findnextitem(tok, possiblepairs, 1) != 'none':
+                possiblepairs[1].append(findnextitem(tok, possiblepairs, 1))
+        print(possiblepairs)
+        if len(possiblepairs[0]) == 3:
+            svopairs.append(possiblepairs[0])
+            possiblepairs[0] = []
+            pendingpairs = pending(possiblepairs)
+        elif len(possiblepairs[1]) == 3:
+           svopairs.append(possiblepairs[1])
+           possiblepairs[1] =[]
+           pendingpairs = pending(possiblepairs)
+    print(svopairs)
     return svopairs
 
 def questionAnalysis(questiondoc):
@@ -112,9 +138,15 @@ def giveAnswerTwo(svo_list, questiondoc):
     questionsvo = questionAnalysis(questiondoc)
     matches = {}
     for i in svo_list:
+        # i[0] = WordNetLemmatizer().lemmatize(i[0],'n')
+        # i[1] = WordNetLemmatizer().lemmatize(i[1],'v')
+        # i[2] = WordNetLemmatizer().lemmatize(i[2],'n')
         matchrating = 0
         possiblematch = []
         for j in questionsvo:
+            # j[0] = WordNetLemmatizer().lemmatize(j[0], 'n')
+            # j[1] = WordNetLemmatizer().lemmatize(j[1], 'v')
+            # j[2] = WordNetLemmatizer().lemmatize(j[2], 'n')
             flaga = True
             flagb = True
             flagc = True
@@ -163,6 +195,11 @@ def giveAnswerTwo(svo_list, questiondoc):
 #            highestmatch = k
 #    return highestmatch
    # return (matches[highestmatch], highestmatch)
+
+#TieBreaker
+def TieBreak():
+    pass
+
 
 #input
 # text = ''
--- venv/Code Testing.py
+++ venv/Code Testing.py
@@ -1,9 +1,17 @@
 import nltk
+import spacy
 from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+nlp = spacy.load("en_core_web_sm")
 syns = wn.synsets("program")
-word1 = wn.synset('people.n.01')
-print(word1)
-word2 = wn.synset('person.n.01')
+# word1 = wn.synset('bad.n.01')
+# word2 = wn.synset('good.n.01')
+# print(word1.wup_similarity(word2))
+# print(word1.path_similarity(word2))
+i = ['president obama', 'accepted', 'the nobel peace prize']
+i[0] = WordNetLemmatizer().lemmatize(i[0],'n')
+i[1] = WordNetLemmatizer().lemmatize(i[1],'v')
+i[2] = WordNetLemmatizer().lemmatize(i[2],'n')
 
-print(word1.wup_similarity(word2))
-print(word1.path_similarity(word2))
+print(i)
